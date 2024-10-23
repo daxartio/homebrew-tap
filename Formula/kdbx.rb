a5ca6948e4a2eec7203b26bee14f8d9fc2e68861
Danil Akhtarov <daxartio@gmail.com>
2024-10-23 11:02:57 +0400

    feat: kdbx 0.9.1

--- Formula/kdbx.rb
+++ Formula/kdbx.rb
@@ -1,31 +1,28 @@
 class Kdbx < Formula
   desc "A secure hole for your passwords (Keepass CLI)."
   homepage "https://github.com/daxartio/kdbx"
-  version "0.9.0"
+  version "0.9.1"
   license "MIT"
-
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.0/kdbx-0.9.0-x86_64-apple-darwin.tar.gz"
-      sha256 "c73d951440c9d45be42a0a2f460d744bc1eca2a2e7656d502a69b163e8f6a54c"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-x86_64-apple-darwin.tar.gz"
+      sha256 "7b769ce1c8a9165caff373c23ab9c58094511b3955ed7a4b006c8b2173fba05d"
     elsif Hardware::CPU.arm?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.0/kdbx-0.9.0-aarch64-apple-darwin.tar.gz"
-      sha256 "9b57b8e235c44a23e7c397d5631c44cfa8e8be1248a2cccd688c9feee6cdb96a"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-aarch64-apple-darwin.tar.gz"
+      sha256 "53011a704cfd88a45574e911b58c83c6ff05cfe297dbed5df309221ae615d234"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.0/kdbx-0.9.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "703b533c7333b8a27b790187b0cd3992de65e3a270c29706c96b899fa49a6dad"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "fd56545d76e12882b61f2ad36e86aa3286995f87caa2b967575d53fe5997ebc4"
     elsif Hardware::CPU.arm?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.0/kdbx-0.9.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "f17823270620d08a967d21d64ae7189822ca7a0ec7e3953f6ccacfe7761a4451"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "25a1dfdbf9eef6051c6706f3f816a16b4553e374b1149db4114406b76a6f658d"
     end
   end
-
   def install
     bin.install "kdbx"
   end
-
   test do
     system "#{bin}/kdbx -V"
   end
