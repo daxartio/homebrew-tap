e1715f402e8ef110a3f6223ca1089a7411ff2394
Danil Akhtarov <daxartio@gmail.com>
2025-01-14 23:13:21 +0400

    feat: kdbx 0.9.3

--- Formula/kdbx.rb
+++ Formula/kdbx.rb
@@ -1,23 +1,23 @@
 class Kdbx < Formula
   desc "A secure hole for your passwords (Keepass CLI)."
   homepage "https://github.com/daxartio/kdbx"
-  version "0.9.1"
+  version "0.9.3"
   license "MIT"
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-x86_64-apple-darwin.tar.gz"
-      sha256 "7b769ce1c8a9165caff373c23ab9c58094511b3955ed7a4b006c8b2173fba05d"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.3/kdbx-0.9.3-x86_64-apple-darwin.tar.gz"
+      sha256 "3aa5e110abea77128ac556be1022e87d4a79735a205dce624dcbc6da90808f55"
     elsif Hardware::CPU.arm?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-aarch64-apple-darwin.tar.gz"
-      sha256 "53011a704cfd88a45574e911b58c83c6ff05cfe297dbed5df309221ae615d234"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.3/kdbx-0.9.3-aarch64-apple-darwin.tar.gz"
+      sha256 "80034b8d0e701ea11bdae657e70e5a96b34701c0bd0461d5e6203fe3f335f6d6"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "fd56545d76e12882b61f2ad36e86aa3286995f87caa2b967575d53fe5997ebc4"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.3/kdbx-0.9.3-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "fca9edb65b93e012e0df8efdd247b7ef8978c5a645d7e61018ca6b28ef69e892"
     elsif Hardware::CPU.arm?
-      url "https://github.com/daxartio/kdbx/releases/download/0.9.1/kdbx-0.9.1-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "25a1dfdbf9eef6051c6706f3f816a16b4553e374b1149db4114406b76a6f658d"
+      url "https://github.com/daxartio/kdbx/releases/download/0.9.3/kdbx-0.9.3-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "91184237763d450ccde4b3824b8faf9e9b39ee3d7d42edbda7236dd812fb99bb"
     end
   end
   def install
