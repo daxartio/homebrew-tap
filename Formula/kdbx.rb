0d0955d782beda88625e228e25ce6a2a9d1579fd
Danil Akhtarov <daxartio@gmail.com>
2026-01-29 22:36:16 +0400

    feat: kdbx 0.12.0

--- Formula/kdbx.rb
+++ Formula/kdbx.rb
@@ -1,23 +1,23 @@
 class Kdbx < Formula
   desc "A secure hole for your passwords (Keepass CLI)."
   homepage "https://github.com/daxartio/kdbx"
-  version "0.11.0"
+  version "0.12.0"
   license "MIT"
   if OS.mac?
     if Hardware::CPU.intel?
-      url "https://github.com/daxartio/kdbx/releases/download/0.11.0/kdbx-0.11.0-x86_64-apple-darwin.tar.gz"
-      sha256 "fa2702ab1932b62ca542a9e14e986b7c2db7bf6df85a4d586e1df18eab527718"
+      url "https://github.com/daxartio/kdbx/releases/download/0.12.0/kdbx-0.12.0-x86_64-apple-darwin.tar.gz"
+      sha256 "3c091b151bf14178f0091d1daffe86795b9e4eb5ed257aec3c257c325fedc861"
     elsif Hardware::CPU.arm?
-      url "https://github.com/daxartio/kdbx/releases/download/0.11.0/kdbx-0.11.0-aarch64-apple-darwin.tar.gz"
-      sha256 "6da3e9e69340081df1cad7dc3dbb9ba948cd2cedc09d188053d3c185f9c6ac62"
+      url "https://github.com/daxartio/kdbx/releases/download/0.12.0/kdbx-0.12.0-aarch64-apple-darwin.tar.gz"
+      sha256 "82a7ea48b0d82e4e4835ecc13711e837ad0ef37fb112c9863c563c21e01bfee3"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/daxartio/kdbx/releases/download/0.11.0/kdbx-0.11.0-x86_64-unknown-linux-musl.tar.gz"
-      sha256 "81e80bee6cac48d478739107bf9488c99851fe2609042b306cbfd9ed6dde7d69"
+      url "https://github.com/daxartio/kdbx/releases/download/0.12.0/kdbx-0.12.0-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "b309df868a207203e62f09a42b024d204c36b113898f36363141032aabe1ebf6"
     elsif Hardware::CPU.arm?
-      url "https://github.com/daxartio/kdbx/releases/download/0.11.0/kdbx-0.11.0-aarch64-unknown-linux-musl.tar.gz"
-      sha256 "cbca6d2821e5bf0ff858bb8cbe3fa344337b6da88e1051cd7fa60f20ec4be7b5"
+      url "https://github.com/daxartio/kdbx/releases/download/0.12.0/kdbx-0.12.0-aarch64-unknown-linux-musl.tar.gz"
+      sha256 "a008ab52ca9c16bd87f312666c6ff9ecbfeb7e5a91c6e043b08ec3d29a229b83"
     end
   end
   def install
